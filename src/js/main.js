ad7a6eca674fc6e6172fc4ad30c61bfd74c95659
Buddhi Ridmal <buddhiridmal@gmail.com>
2025-03-17 11:30:28 +0530

    Task UI implementation

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+const taskItem = $(".task-item");
+
+for (let i = 0; i < 5; i++) {
+    $("#tasks-list").append(taskItem.clone());
+    $("#completed-task-list").append(taskItem.clone());
+}
